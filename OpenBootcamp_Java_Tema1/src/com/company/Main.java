f8052a206b1ec7b3226241c33cfde8f812e394f3
Carlos Rodriguez Gomez <62936262+carlitosrogo@users.noreply.github.com>
2022-10-25 20:36:22 +0200

    Add files via upload

---
+++ OpenBootcamp_Java_Tema1/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int auxInt = 5;
+        long auxLong = 1231241991;
+        double auxDouble = 123.123;
+        boolean auxBoolean = true;
+        String auxString = "Hola me llamo Carlos";
+        char auxChar = 'a';
+
+        System.out.println("Nuestro integer es: "+ auxInt);
+        System.out.println("Nuestro long es: "+ auxLong);
+        System.out.println("Nuestro double es: "+ auxDouble);
+        System.out.println("Nuestro boolean es: "+ auxBoolean);
+        System.out.println("Nuestro String es: "+ auxString);
+        System.out.println("Nuestro char es: "+ auxChar);
+
+    }
+}
